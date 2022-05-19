80109da78f269110866fc6276d3a7ec5781a17a9
Alastor2189 <g.lucafarci89@gmail.com>
2022-05-19 18:39:53 +0200

    inserimento header

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+      <img src="../assets/logo.svg" alt="logo spotify">
+  </header>
+</template>
+
+<script>
+export default {
+    name: "AppHeader",
+}
+</script>
+
+<style>
+    .header{
+        padding: 0.5em;
+        background-color: #2e3a46;
+        height: 4em;
+        width: 100%;
+        display: flex;
+        align-items: center;   
+    }
+    img {
+        height: 40px;
+        }
+</style>

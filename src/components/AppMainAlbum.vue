2ec51fafd4f7dcd026cbfe6891b246a9197bb382
Alastor2189 <g.lucafarci89@gmail.com>
2022-05-20 18:55:00 +0200

    inserimento funzioni, ma incorrette

--- src/components/AppMainAlbum.vue
+++ src/components/AppMainAlbum.vue
@@ -1,5 +1,8 @@
 <template>
   <div class="container">
+    <div class="research">
+      <AppResearch @searchClic="outputDate($event)"/>
+    </div>
     <div class="row row-cols-5">
           <AppCd v-for="(item, index) in coverCd" :key="index" :album="item"/>
     </div>    
@@ -16,9 +19,24 @@ export default {
     AppCd,
     AppResearch,
   },
+  computed: {
+    cdFilter() {
+      const filterArray = this.coverCd.filter((item) => {
+        return item.genre.toLowerCase().includes(this.wordResearch)
+      })
+      return filterArray
+    }
+  },
+
+  methods: {
+    outputDate(word) {
+      this.wordResearch = word.toLowerCase();
+    }
+  },
   data: function(){
     return {
       coverCd:[],
+      wordResearch:"",
     };
   },
   created() {
--- src/components/AppResearch.vue
+++ src/components/AppResearch.vue
@@ -1,13 +1,30 @@
 <template>
-  <div>
-
+  <div class="search">
+      <select name="" id="" v-model="research" @change="$emit('searchClic','research')">
+            <option value="">All</option>
+            <option value="rock">Rock</option>
+            <option value="pop">Pop</option>
+            <option value="jazz">Jazz</option>
+            <option value="metal">Metal</option>
+      </select>
+      
   </div>
 </template>
 
 <script>
 export default {
     name: "AppResearch",
-}
+    data: function(){
+         return {
+            research:"",
+        };
+        
+        },
+        methods: {
+            
+        },
+    
+};
 </script>
 
 <style>
